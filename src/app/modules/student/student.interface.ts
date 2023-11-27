a5df0b01b30ecebe93e20558ce7c1528287e9e97
apurbojamananis <apurbojaman.anis@gmail.com>
2023-11-27 22:56:06 +0600

    solve some errors

---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,38 @@
+export type Username = {
+  firstName: string;
+  middleName?: string;
+  lastName: string;
+};
+
+export type Guardian = {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactNo: string;
+};
+
+export type LocalGuardian = {
+  Name: string;
+  Occupation: string;
+  ContactNo: string;
+  address: string;
+};
+
+export type Student = {
+  id: string;
+  name: Username;
+  gender: 'male' | 'female';
+  dataOfBirth?: string;
+  email: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: Guardian;
+  localGuardian: LocalGuardian;
+  profileImg?: string;
+  isActive: 'active' | 'block';
+};
